1ae74aec3d92c6a404d5804d4f3f11efd883a305
yunomer <noonnotnoon@gmail.com>
2022-03-28 17:19:09 -0400

    Adding Redux

---
+++ src/services/cryptoAPIs.js
@@ -0,0 +1,17 @@
+var options = {
+  method: 'GET',
+  url: 'https://coinranking1.p.rapidapi.com/coins',
+  params: {
+    referenceCurrencyUuid: 'yhjMzLPhuIDl',
+    timePeriod: '24h',
+    tiers: '1',
+    orderBy: 'marketCap',
+    orderDirection: 'desc',
+    limit: '50',
+    offset: '0'
+  },
+  headers: {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': '7383fbb166msh4f115b6fe77d068p163335jsn5173e31be8dd'
+  }
+};
